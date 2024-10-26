2f7dd17cf132bf19637f486ba690ba4bc22c937a
Hajar <hajarrashidi@gmail.com>
2024-10-26 18:20:23 +0200

    spring boot init with deps: actuator, data-jpa, security, web, devtools, postgresql, starter, security..

---
+++ backend/hello-backend/src/main/java/com/veycloud/hellobackend/HelloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.veycloud.hellobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloBackendApplication.class, args);
+    }
+
+}
---
+++ backend/hello-backend/src/test/java/com/veycloud/hellobackend/HelloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veycloud.hellobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
